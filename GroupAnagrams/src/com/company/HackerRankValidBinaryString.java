1b21b137447531c4d1c909e69601d160a9d85bda
bashiulAlam <sabab.ndc@gmail.com>
2020-09-23 15:47:57 +0600

    implemented hacker rank valid binary strings, not accepted

---
+++ GroupAnagrams/src/com/company/HackerRankValidBinaryString.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HackerRankValidBinaryString {
+    public int minimumMoves(String s, int d) {
+        int minimumMoves = 0;
+
+        for (int i = 0; i <= s.length() - d; i++) {
+            String subStr = s.substring(i, i + d);
+            if (!subStr.contains("1")) {
+                minimumMoves++;
+                char[] c = s.toCharArray();
+                c[subStr.length() - 1] = '1';
+                s = String.valueOf(c);
+                System.out.println("changed string " + s + ", for " + subStr);
+            }
+        }
+
+        return minimumMoves;
+    }
+
+    public List<String> findSubStringOfGivenLength(String s, int d) {
+        List<String> subStrs = new ArrayList<String>();
+
+        for (int i = 0; i <= s.length() - d; i++) {
+            //for (int j = i + 1; j <= d; j++)
+            subStrs.add(s.substring(i, i + d));
+        }
+
+        //System.out.println("subStrs size " + subStrs.size());
+        //System.out.println("subStrs " + subStrs);
+
+        return subStrs;
+    }
+}
--- GroupAnagrams/src/com/company/Main.java
+++ GroupAnagrams/src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        GroupAnagrams groupAnagrams = new GroupAnagrams();
+        /*GroupAnagrams groupAnagrams = new GroupAnagrams();
         String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
         //String[] strs = new String[] {"", ""};
         //System.out.println(groupAnagrams.groupAnagrams(strs));
@@ -16,6 +16,9 @@ public class Main {
             for (int j = 0; j < anagrams.get(i).size(); j++)
                 System.out.println(" item : " + anagrams.get(i).get(j));
             System.out.println();
-        }
+        }*/
+
+        HackerRankValidBinaryString validBinaryString = new HackerRankValidBinaryString();
+        System.out.println(validBinaryString.minimumMoves("00011111", 2));
     }
 }
