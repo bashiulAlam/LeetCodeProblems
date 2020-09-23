6b358ead4d8f33ee7c0f63b16bef24e0a69b26d3
bashiulAlam <sabab.ndc@gmail.com>
2020-09-23 14:49:27 +0600

    while loop added for group anagrams

--- GroupAnagrams/src/com/company/GroupAnagrams.java
+++ GroupAnagrams/src/com/company/GroupAnagrams.java
@@ -1,22 +1,28 @@
 package com.company;
 
+import com.sun.deploy.util.ArrayUtil;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import org.apache.commons.lang3.ArrayUtils;
 
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> anagrams = new ArrayList<List<String>>();
-        List<String> item = new ArrayList<String>();
+        List<String> emptyStrList = new ArrayList<String>();
         String currentStr = "";
+        int i = 0;
 
-        for (int i = 0; i < strs.length; i++) {
+        while (strs.length > 0) {
             if (!strs[i].isEmpty()) {
+                List<String> item = new ArrayList<String>();
                 item.add(strs[i]);
                 System.out.println("item added from index : " + i + " : " + item.get(item.size() - 1));
                 currentStr = strs[i];
-                strs[i] = "";
-                System.out.println("comparing string : " + currentStr);
+                strs = ArrayUtils.remove(strs, i);
+                //System.out.println("comparing string : " + currentStr);
                 char[] currentChar = currentStr.toCharArray();
                 Arrays.sort(currentChar);
                 for (int j = i + 1; j < strs.length; j++) {
@@ -24,17 +30,25 @@ public class GroupAnagrams {
                     Arrays.sort(c);
                     if (String.valueOf(c).equals(String.valueOf(currentChar))) {
                         item.add(strs[j]);
-                        System.out.println("item added from index : " + j + " : " + item.get(item.size() - 1));
-                        System.out.println("item size : " + item.size());
-                        strs[j] = "";
+                        //System.out.println("item added from index : " + j + " : " + item.get(item.size() - 1));
+                        //System.out.println("item size : " + item.size());
+                        strs = ArrayUtils.remove(strs, j);
                     }
                 }
 
                 anagrams.add(item);
-                System.out.println("anagrams size : " + anagrams.size() + " last item " + anagrams.get(anagrams.size() - 1).get(0));
-                item.clear();
+                System.out.println("anagrams size : " + anagrams.size() + " last item " + anagrams.get(anagrams.size() - 1));
+            } else {
+                emptyStrList.add("");
+                strs = ArrayUtils.remove(strs, i);
+                //System.out.println("emptyStrList size : " + emptyStrList.size());
             }
+
+            i++;
         }
+
+        if (emptyStrList.size() > 0)
+            anagrams.add(emptyStrList);
 
         return anagrams;
     }
--- GroupAnagrams/src/com/company/Main.java
+++ GroupAnagrams/src/com/company/Main.java
@@ -1,18 +1,20 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         GroupAnagrams groupAnagrams = new GroupAnagrams();
-        String[] strs = new String[] {"eat","tea","tan","ate","nat","bat"};
+        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        //String[] strs = new String[] {"", ""};
         //System.out.println(groupAnagrams.groupAnagrams(strs));
         List<List<String>> anagrams = groupAnagrams.groupAnagrams(strs);
         for (int i = 0; i < anagrams.size(); i++) {
             for (int j = 0; j < anagrams.get(i).size(); j++)
-                System.out.println(anagrams.get(i).get(j));
+                System.out.println(" item : " + anagrams.get(i).get(j));
             System.out.println();
         }
     }
