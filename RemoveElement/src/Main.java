a9cf294c9f1319f76607e844a6d8f4513a34087f
bashiulAlam <sabab.ndc@gmail.com>
2020-09-30 18:25:31 +0600

    solved remove element problem

---
+++ RemoveElement/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        RemoveElement removeElement = new RemoveElement();
+        int[] arr = new int[] {3,2,2,3};
+        //int[] arr = new int[] {1};
+        System.out.println(removeElement.removeElement(arr, 3));
+    }
+}
