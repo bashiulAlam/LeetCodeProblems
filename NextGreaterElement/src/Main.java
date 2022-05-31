3c792b11381c9277a428d51a9ad65a0bebc5ac2a
bashiulAlam <sabab.ndc@gmail.com>
2022-05-31 18:44:36 +0600

    solved next greater element II

--- NextGreaterElement/src/Main.java
+++ NextGreaterElement/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
@@ -25,6 +27,33 @@ public class Main {
         return result;
     }
 
+    public static int[] nextGreaterElements(int[] nums) {
+        int[] result = new int[nums.length];
+        Arrays.fill(result, Integer.MIN_VALUE);
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = i + 1;
+            int traverseCount = 0;
+            while (traverseCount < nums.length && index != i) {
+                if (index >= nums.length)
+                    index = 0;
+
+                if (nums[index] > nums[i]) {
+                    result[i] = nums[index];
+                    System.out.println("setting next greater for " + nums[i] + " as " + nums[index]);
+                    break;
+                } else index++;
+
+                traverseCount++;
+            }
+
+            if (result[i] == Integer.MIN_VALUE)
+                result[i] = -1;
+        }
+
+        return result;
+    }
+
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
@@ -33,9 +62,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        printArray(nextGreaterElement(new int[] {4,1,2}, new int[] {1,3,4,2}));
+        /*printArray(nextGreaterElement(new int[] {4,1,2}, new int[] {1,3,4,2}));
         printArray(nextGreaterElement(new int[] {2,4}, new int[] {1,2,3,4}));
         printArray(nextGreaterElement(new int[] {3,1,5,7,9,2,6}, new int[] {1,2,3,5,6,7,9,11}));
-        printArray(nextGreaterElement(new int[] {1,3,5,2,4}, new int[] {6,5,4,3,2,1,7}));
+        printArray(nextGreaterElement(new int[] {1,3,5,2,4}, new int[] {6,5,4,3,2,1,7}));*/
+
+        //printArray(nextGreaterElements(new int[] {1,2,1}));
+        printArray(nextGreaterElements(new int[] {1,2,3,4,3}));
     }
 }
