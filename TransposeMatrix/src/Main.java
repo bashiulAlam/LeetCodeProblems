f70c4973ca5fd16fbc0067515b35e490f601ae81
bashiulAlam <sabab.ndc@gmail.com>
2022-06-02 20:59:18 +0600

    solved transpose matrix

---
+++ TransposeMatrix/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static int[][] transpose(int[][] matrix) {
+        int[][] result = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public static void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        print(transpose(new int[][] {{1,2,3}, {4,5,6}}));
+    }
+}
